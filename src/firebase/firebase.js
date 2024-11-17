c588d552963df219ecba5bb598eb0eb48123a12f
Akash Mishra <114410655+akumi07@users.noreply.github.com>
2024-11-17 19:41:53 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 // Your Firebase configuration
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBbXZzg88EHdH8ViGSxbnuB7Uk2qMfo-Dw",
+  authDomain: "car-management-system-f28a9.firebaseapp.com",
+  projectId: "car-management-system-f28a9",
+  storageBucket: "car-management-system-f28a9.firebasestorage.app",
+  messagingSenderId: "253642447112",
+  appId: "1:253642447112:web:3fb408d0c4828d8093b4bd",
+  measurementId: "G-R2P59T9X12"
 };
 
 
