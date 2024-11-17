a815890f8636c6db1ec293c0f133e2af39640326
Akash Mishra <114410655+akumi07@users.noreply.github.com>
2024-11-17 18:30:41 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,24 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getStorage } from 'firebase/storage';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "car-management-system-f28a9.firebaseapp.com",
-  projectId: "car-management-system-f28a9",
-  storageBucket: "car-management-system-f28a9.firebasestorage.app",
-  messagingSenderId: "253642447112",
-  appId: "1:253642447112:web:3fb408d0c4828d8093b4bd",
-  measurementId: "G-R2P59T9X12"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const storage = getStorage(app);
 const db = getFirestore(app);
-export {app,auth,storage, db, addDoc, collection};
-
-
-
-// const auth = getAuth(app);
+const storage = getStorage(app);
+export default {app,auth,db,storage};
