5697650b153b8bc5435c9641d0e2f8bb2170ed1b
Akash Mishra <114410655+akumi07@users.noreply.github.com>
2024-11-17 19:15:52 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -21,4 +21,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-export default {app,auth,db,storage};
+export { auth, db, storage };
